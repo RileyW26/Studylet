e58f1395080f611d76e9bb1a8dc314866af597a9
RyanTandean <129213323+RyanTandean@users.noreply.github.com>
2023-05-30 13:06:43 -0400

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -74,6 +74,7 @@ def makeMultipleChoiceQuestions(dividedQuestions, qt):
             multipleAnswers.append(definitions[randomNumber])
         multipleChoiceQuestions.update({terms[multipleChoice[i]] : multipleAnswers })
     print(multipleChoiceQuestions)
+    return multipleChoiceQuestions
 
 
 td = openFile()
@@ -82,3 +83,35 @@ splitedQuestions = splitQuestions(td2)
 questionTF = makeTrueOrFalseQuestions(splitedQuestions, td2)
 questionMC = makeMultipleChoiceQuestions(splitedQuestions, td2)
 
+from tkinter import * 
+from tkinter.ttk import *
+
+# create main window 
+root = Tk()
+
+root.title("Text")
+root.attributes('-fullscreen', True)
+
+# Labe lis what output will be 
+# show on window
+#label = Label(root, text = "Hello World !").pack()
+
+def newWindow():
+    # Toplevel object which will be treated as a new window
+    newWindow = Toplevel(root)
+
+    newWindow.title("new")
+
+    newWindow.attributes('-fullscreen', True)
+
+    root.withdraw()
+
+
+btn = Button(root, text = 'Exit',
+             command = root.destroy)#Exit 
+studysetbtn = Button(root, text = 'Study sets',
+                     command = lambda:[newWindow()])
+btn.pack(side = 'right')
+studysetbtn.pack(side = 'left')
+# calling mainloop method which is used when you rapplication is ready to run and it tells the code to keep displaying
+mainloop()
