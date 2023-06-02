64393c61a851badd6d185c88d54add83ae3de0c9
RyanTandean <129213323+RyanTandean@users.noreply.github.com>
2023-06-02 13:50:53 -0400

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -4,6 +4,7 @@ import csv
 import os
 from os.path import exists
 import random
+import time
 def openFile():
     fileName = os.getcwd() + '\\Studylet\\test.csv'
     file = open(fileName, "r")
@@ -34,12 +35,12 @@ def splitQuestions(questionTerms):
         randomInteger = random.choice(questions)
         questions.remove(randomInteger)
         trueOrFalseQuestions.append(randomInteger)
+    print("2: ", questions, trueOrFalseQuestions)
     return questions, trueOrFalseQuestions
 def makeTrueOrFalseQuestions(dividedQuestions, qt):
     trueOrFalseWrong = []
     trueOrFalseQuestion = {}
     trueOrFalseWrongQuestion = {}
-
     multipleChoice, trueOrFalse = dividedQuestions
     terms, definitions = qt
     halfLength = len(trueOrFalse)/2
@@ -48,16 +49,19 @@ def makeTrueOrFalseQuestions(dividedQuestions, qt):
     for i in range(int(halfLength)):
         randomInteger = random.choice(trueOrFalse)
         trueOrFalse.remove(randomInteger)
-        trueOrFalseWrong.append(randomInteger)
+
     for j in range (len(trueOrFalse)):
         trueOrFalseQuestion.update({terms[trueOrFalse[j]] : definitions[trueOrFalse[j]]})
+
     for k in range (len(trueOrFalseWrong)):
         randomElement = random.randint(0, len(definitions)-1)
         excludedElement = trueOrFalseWrong[k]
+        print(excludedElement)
         while randomElement == excludedElement:
             randomElement = random.choice(trueOrFalseWrong)
         trueOrFalseWrongQuestion.update({terms[trueOrFalseWrong[k]] : definitions[randomElement]})
-        return trueOrFalseQuestion, trueOrFalseWrongQuestion
+    print(trueOrFalseQuestion, trueOrFalseWrongQuestion)
+    return trueOrFalseQuestion, trueOrFalseWrongQuestion
 def makeMultipleChoiceQuestions(dividedQuestions, qt):
     multipleChoice, trueOrFalse = dividedQuestions
     terms, definitions = qt
@@ -77,21 +81,27 @@ def makeMultipleChoiceQuestions(dividedQuestions, qt):
         multipleChoiceQuestions.update({terms[multipleChoice[i]] : multipleAnswers })
     return multipleChoiceQuestions
 
-quizCounter = 0
 td = openFile()
 td2 = seperateTermsDefinitions(td)
 splitedQuestions = splitQuestions(td2)
-questionTF = makeTrueOrFalseQuestions(splitedQuestions, td2)
+questionTF, questionTFWrong = makeTrueOrFalseQuestions(splitedQuestions, td2)
 questionMC = makeMultipleChoiceQuestions(splitedQuestions, td2)
-
-def quizMenu(questionTF, questionMC):
+print("splitedQuestions: ", splitedQuestions)
+print("questionTF: ", questionTF, questionTFWrong)
+print('questionMC: ', questionMC)
+def quizMenu(questionTF, questionMC, window):
     # Toplevel object which will be treated as a new window
-    quizMenu = Toplevel(root)
+    
+    '''quizMenu = Toplevel(root)
 
     quizMenu.title('Quiz Home Menu')
 
     quizMenu.attributes('-fullscreen', True)
-
+'''
+    canvas = Canvas(window)
+    Canvas.pack(side=LEFT, fill=BOTH, expand=True)
+    frame = Frame(canvas)
+    
     title = "Quiz Menu"
     titlelbl = Label(quizMenu, text = title)
 
@@ -99,20 +109,15 @@ def quizMenu(questionTF, questionMC):
     titlelbl.place(anchor = CENTER, relx = .5, rely = .2)
     startbtn.place(anchor = CENTER, relx = .5, rely = .5)
     root.withdraw()  # closes the root window
-def answerSubmitted(answerSubmission):
-    print(answerSubmission)
 
-    # Store the result in a StringVar
-    #result_var.set(str(result))
-def check_button(button_pressed, quiz):
-    if button_pressed.get():
-        print("Button Pressed")
-    else:
-        quiz.after(100, check_button, button_pressed, quiz)
 def quiz_window(quizMenu, questionTF, questionMC):
     #Visuals
     quizMenu.destroy()
-    while questionMC !={}:
+    score = 0
+    fullyCorrectScore = len(list(questionMC))
+    print(fullyCorrectScore)
+    start = time.time()
+    while questionMC != {}:
         quiz = Toplevel()
 
         quiz.title('Quiz')
@@ -126,16 +131,12 @@ def quiz_window(quizMenu, questionTF, questionMC):
         listQuestionMC = dict(questionMC)
 
         question = random.choice(list(questionMC)) #random term to act as question
-        print(questionMC)
-        print("question: ", question)
         questionMC.pop(question)
-        print(questionMC)
         
 
         answer = listQuestionMC.get(question)
         correctAnswer = answer[0]
         random.shuffle(answer)
-        print(answer)
         print("correct: ", correctAnswer)
         questionDescription = Label(quiz, text = "Multiple Choice: Choose the matching definition")
         questionDescription.place(anchor = CENTER, relx = .5, rely = .2)
@@ -144,16 +145,25 @@ def quiz_window(quizMenu, questionTF, questionMC):
         questionDisplay = Label(quiz, text = question)
         questionDisplay.place(anchor = CENTER, relx = .5, rely = .3)
         button_pressed = BooleanVar()
-        answerOneBtn = Button(quiz, text = answer[0], command=lambda id = answer[0]:[answerSubmitted(id), button_pressed.set(True)])
-        answerTwoBtn = Button(quiz, text = answer[1], command=lambda id = answer[1]:[answerSubmitted(id)])
-        answerThreeBtn = Button(quiz, text = answer[2], command=lambda id = answer[2]:[answerSubmitted(id)])
-        answerFourtBtn = Button(quiz, text = answer[3], command=lambda id = answer[3]:[answerSubmitted(id)])
+        answer_submitted = StringVar()
+        answerOneBtn = Button(quiz, text = answer[0], command=lambda id = answer[0]:[answer_submitted.set(id), button_pressed.set(True)])
+        answerTwoBtn = Button(quiz, text = answer[1], command=lambda id = answer[1]:[answer_submitted.set(id), button_pressed.set(True)])
+        answerThreeBtn = Button(quiz, text = answer[2], command=lambda id = answer[2]:[answer_submitted.set(id), button_pressed.set(True)])
+        answerFourthBtn = Button(quiz, text = answer[3], command=lambda id = answer[3]:[answer_submitted.set(id), button_pressed.set(True)])
         answerOneBtn.place(anchor = CENTER, relx = .3, rely = .5)
         answerTwoBtn.place(anchor = CENTER, relx = .7, rely = .5)
         answerThreeBtn.place(anchor = CENTER, relx = .3, rely = .6)
-        answerFourtBtn.place(anchor = CENTER, relx = .7, rely = .6)
-        quiz.after(100, check_button, button_pressed, quiz)
-        print('works')
+        answerFourthBtn.place(anchor = CENTER, relx = .7, rely = .6)
+        quiz.wait_variable(button_pressed)
+    
+        if answer_submitted.get() == correctAnswer:
+            print("correct") 
+            score += 1
+        quiz.destroy()
+    end = time.time()
+    print('int score: ', score)
+    print("Time taken to complete the quiz: ", end-start, "seconds")
+    print("score: ", str((score/fullyCorrectScore)*100) + "%")
 
 
 
@@ -163,7 +173,7 @@ def homeMenu(questionTF, questionMC):
     titlelbl = Label(root, text = title)
     exitbtn = Button(root, text = 'Exit',
                 command = root.destroy)#Exit 
-    quizbtn = Button(root, text = 'Quiz', command = lambda:[quizMenu(questionTF, questionMC)])
+    quizbtn = Button(root, text = 'Quiz', command = lambda:[quizMenu(questionTF, questionMC, root)])
     
     quizbtn.place(anchor = CENTER, relx = 0.5, rely = 0.5)
 
@@ -171,6 +181,7 @@ def homeMenu(questionTF, questionMC):
     mainloop()
 
 root = Tk()
+
 
 root.title("Studylet")
 root.attributes('-fullscreen', True)
