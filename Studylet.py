13c1ec993e3a63195869c24cb6eedf22ba80cc69
RileyW26 <129102618+RileyW26@users.noreply.github.com>
2023-06-06 13:01:06 -0400

    update

--- Studylet.py
+++ Studylet.py
@@ -23,20 +23,55 @@ def flashcardHome(window):
     canvas.create_window((canvas_width, canvas_height), window=frame, anchor=CENTER)
     title = "Flashcard Menu"
     titlelbl = Label(frame, text = title)
-    playbtn = Button(frame, text = 'Play flashcard')
-    '''
-    addbtn = Button(frame, text = 'Add flashcard',
-                    command = lambda:[addFlashcard(frame)])
-    removebtn = Button(frame, text = 'Remove flashcard',
-                       command = lambda:[removeMenu(frame)])
-    '''
+    playbtn = Button(frame, text = 'Play flashcard',
+                     command = lambda:[flashcardList(canvas)])
     backbtn = Button(frame, text = 'Back',
                      command = lambda:[homeMenu(canvas)])
     
     titlelbl.pack()
     playbtn.pack()
     backbtn.pack()
+def flashcardList(window):
+    folder = os.getcwd()
+    file = folder + "\\Studysets.csv"
+    f = open(file, "r")
+    lines = len(f.readlines())
+    canvas_width_percentage = 50 # Width as a percentage of the window width
+    canvas_height_percentage = 20  # Height as a percentage of the window height
+
+    # Calculate the pixel values based on percentages
+    window_width = window.winfo_screenwidth()
+    window_height = window.winfo_screenheight()
+    canvas_width = percentageWindow(canvas_width_percentage, window_width)
+    canvas_height = percentageWindow(canvas_height_percentage, window_height)
+    # Create a Canvas widget
+    canvas = Canvas(window)
+    canvas.pack(side=LEFT, fill=BOTH, expand=True)
+
+    # Create a Scrollbar widget
+    scrollbar = Scrollbar(root, orient=VERTICAL, command=canvas.yview)
+    scrollbar.pack(side=RIGHT, fill=Y)
+
+    # Configure the Canvas to use the Scrollbar
+    canvas.configure(yscrollcommand=scrollbar.set)
+
+    # Create a Frame inside the Canvas
+    frame = Frame(canvas)
+    canvas.create_window((canvas_width, canvas_height), window=frame, anchor=NW)
+
+    titlelbl = Label(frame, text = 'Available Studysets')
+    titlelbl.pack()
+    backbutton = Button(frame, text = "back",
+                        command = lambda:[flashcardHome(canvas), toggle_scrollbar(scrollbar)])
     
+    # Placing buttons
+    for i in range(lines):
+            button = Button(frame, text = titles(i),
+                            command = lambda id = i:[flashcard(canvas), toggle_scrollbar(scrollbar)])
+            button.pack(side = "top", fill = X)
+    backbutton.pack()
+def flashcard(window):
+    pass
 def quizMenu(window):
     window.delete('all')
     # Toplevel object which will be treated as a new window
@@ -231,6 +266,9 @@ def addFlashcard2(window):
     addbtn.pack()
     backbtn.pack()
 def addData2(term, definition):
+    '''
+    Appends a term and definition onto studysets.csv, pulling them from entries 
+    '''
     term = term.get()
     definition = definition.get()
     folder = os.getcwd()
@@ -271,7 +309,7 @@ def removeMenu(window):
     canvas.create_window((canvas_width, canvas_height), window=frame, anchor=NW)
 
     titlelbl = Label(frame, text = 'Remove Available Studysets')
-    titlelbl.pack
+    titlelbl.pack()
     backbutton = Button(frame, text = "back",
                         command = lambda:[studysetMenu(canvas), toggle_scrollbar(scrollbar)])
     
@@ -295,6 +333,9 @@ def titles(lines):
     file.close()
     return title
 def seperateTermsDefinitions(termsAndDefinitionsValues):
+    '''
+    Returns terms and definitions
+    '''
     terms = []
     definitions = []
     for i in range (1, len(termsAndDefinitionsValues)):
@@ -306,8 +347,15 @@ def seperateTermsDefinitions(termsAndDefinitionsValues):
     #print(definitions)
     return terms, definitions
 def percentageWindow(percentage, total_length):
+    '''
+    Creates percentages of the screen, allowing percentages to be applied to frames on a canvas
+    '''
     return int(percentage / 100 * total_length)
 def removeSet(window, num):
+    '''
+    Screen that shows list of terms and definitions on a studyset, in an entry box where the user can modify them as they would like.  
+    They can save the entries, or remove the entire studyset
+    '''
     window.delete('all')
    
     fileName = os.getcwd() + '\\Studysets.csv'
@@ -350,26 +398,34 @@ def removeSet(window, num):
     
     frame3 = Frame(canvas)
     canvas.create_window((canvas_width3, canvas_height3), window=frame3, anchor = CENTER)
+
+    frame4 = Frame(canvas)
+    canvas.create_window((canvas_width3, canvas_height), window = frame4, anchor = CENTER)
     termlbl = Label(frame, text = 'Terms:')
     deflbl = Label(frame2, text = 'Definitions:')
     termlbl.pack()
     deflbl.pack()
     termEntry = []
     defEntry = []
+    titlelbl = Label(frame3, text = 'Edit studyset "' + titles(num)+'"')
+    titlelbl.pack()
+    blank = Label(frame4, text = '')
+    blank.pack()
+    file.close()
     for i in range(len(terms)):
         entry = Entry(frame)
         entry.insert(END, terms[i])
         entry.pack()
         termEntry.append(entry)
+        blank = Label(frame4, text = '')
+        blank.pack()
     for i in range(len(defintions)):
         entry2 = Entry(frame2)
         entry2.insert(END, defintions[i])
         entry2.pack()
         defEntry.append(entry2)
-    titlelbl = Label(frame3, text = 'Edit studyset "' + titles(num)+'"')
-    titlelbl.pack()
-    savebtn = Button(frame3, text = 'Save',
-                     command = lambda:[save(termEntry, defEntry, num),toggle_scrollbar(scrollbar),removeLine(num)])
+    savebtn = Button(frame4, text = 'Save',
+                     command = lambda:[save(termEntry, defEntry, num),toggle_scrollbar(scrollbar), removeMenu(canvas)  ])
     savebtn.pack()
     backbtn = Button(frame, text = 'Back',
                      command = lambda:[removeMenu(canvas),toggle_scrollbar(scrollbar)])
@@ -377,30 +433,46 @@ def removeSet(window, num):
     removebtn = Button(frame2, text = "Remove Entire Studyset",
                        command = lambda:[removeLine(num),toggle_scrollbar(scrollbar), removeMenu(canvas)])
     removebtn.pack()
-    file.close()
 def save(term, definition, num):
+    '''
+    appends the term and definition values pulled from entries that the user can edit 
+    '''
     title = titles(num)
     fileName = os.getcwd() + '\\Studysets.csv'
     file = open(fileName, "a")
-    file.write("\n" + title)
+    
+    file.write(title)
     for i in range(len(term)):
         t = term[i].get()
         d = definition[i].get()
-        file.write("|" + t + "|" + d)
+        stripd = d.replace('\n', '')
+        file.write("|" + t + "|" + stripd)
     file.close()
-
+    removeLine(num)
+def strip(line):
+    '''
+    Strips lines of the line break
+    '''
+    newLine = line.replace('\n', '')
+    return newLine
 def removeLine(num):
+    '''
+    Function that removes a line
+    '''
     num = num + 1
     fileName = os.getcwd() + '\\Studysets.csv'
     remove_line_from_csv(fileName, num)
 def remove_line_from_csv(file_path, line_number):
+    '''
+    Removes a line from a csv
+    '''
     file = open(file_path, 'r')
     lines = file.readlines()
     file.close()
     filew = open(file_path, 'w', newline = '')
     for index, line in enumerate( lines, start = 1):
         if index != line_number:
-            filew.write(line)
+            filew.write(strip(line) + '\n')
     filew.close()
 def toggle_scrollbar(scrollbar):
     '''
@@ -408,6 +480,9 @@ def toggle_scrollbar(scrollbar):
     '''
     scrollbar.pack_forget()
 def addExisting(window):
+    '''
+    Window that gives the user their current direcctory and has an entry box for a csv file to be input
+    '''
     canvas_width_percentage = 50 # Width as a percentage of the window width
     canvas_height_percentage = 40  # Height as a percentage of the window height
 
@@ -426,7 +501,7 @@ def addExisting(window):
     currentdirectory = Label(frame, text = "Directory: " + os.getcwd())
     csventry = Entry(frame)
     addbtn = Button(frame, text = 'Add',
-                    command = lambda:[addCsv(csventry)])
+                    command = lambda:[addCsv(csventry),studysetMenu(canvas)])
     backbtn = Button(frame, text = 'Back',
                      command = lambda:[studysetMenu(canvas)])
     titlelbl.pack()
@@ -435,15 +510,16 @@ def addExisting(window):
     addbtn.pack()
     backbtn.pack()
 def addCsv(entry):
+    '''
+    reads the csv that the user inputted and appends it onto the studysets.csv that the programs reads
+    '''
     fileName = os.getcwd() + "\\" + entry.get()
     file = open(fileName, "r")
     text = file.readlines()
-    print(text)
     file.close()
     studysets = os.getcwd() + "\\Studysets.csv"
     file = open(studysets, "a")
-    file.writelines("\n")
-    file.writelines(text) 
+    file.writelines(text + "\n") 
 # create main window 
 root = Tk()
 
