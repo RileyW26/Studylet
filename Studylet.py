6433c7b0f222f5fded2fe51a9ef2d38095acd840
RyanTandean <129213323+RyanTandean@users.noreply.github.com>
2023-06-09 13:11:36 -0400

    leaderboard done

--- Studylet.py
+++ Studylet.py
@@ -39,18 +39,33 @@ def flashcardHome(window):
     titlelbl.pack()
     playbtn.pack()
     backbtn.pack()
+
 def seperateTermsDefinitions(termsAndDefinitionsValues):
-    terms = []
-    definitions = []
-    for i in range (1, len(termsAndDefinitionsValues)):
-        if i % 2 == 0:
+    '''
+    This function takes a list of the title, terms and definitions
+
+    Sorts the terms and definitions into two seperate lists, and returns the two lists, discarding the title
+    
+    '''
+    print(termsAndDefinitionsValues)
+    terms = []#declare list
+    definitions = []#declare list
+    for i in range (1, len(termsAndDefinitionsValues)):#start from 1 to ignore title
+        if i % 2 == 0: #if index is even, it is a definition
             definitions.append(termsAndDefinitionsValues[i])
-        elif i % 2 != 0: 
+        elif i % 2 != 0: #if index is even, it is a term
             terms.append(termsAndDefinitionsValues[i])
-    #print(terms)
-    #print(definitions)
     return terms, definitions
+
 def splitQuestions(questionTerms):
+    '''
+    Takes the two lists terms and definitions
+
+    splits the number of terms into two
+    
+    
+    
+    '''
     terms, definitions = questionTerms
     questions = []
     trueOrFalseQuestions = []
@@ -63,6 +78,7 @@ def splitQuestions(questionTerms):
         questions.remove(randomInteger)
         trueOrFalseQuestions.append(randomInteger)
     return questions, trueOrFalseQuestions
+
 def makeTrueOrFalseQuestions(dividedQuestions, qt):
     trueOrFalseWrong = []
     trueOrFalseQuestion = {}
@@ -120,6 +136,17 @@ def quizChecker(termsAndDefinitions):
     if len(termsAndDefinitions[0]) < 4:
         messagebox.showinfo("Quiz Requirements", "To start a quiz with this study set, make sure there is atleast 4 pairs of terms and definitions")
         return True
+def frames(canvas, window):
+    frame = Frame(canvas)
+    # Calculate the pixel values based on percentages
+    canvas_width_percentage = 50  # Width as a percentage of the window width
+    canvas_height_percentage = 50  # Height as a percentage of the window height
+    window_width = window.winfo_screenwidth()
+    window_height = window.winfo_screenheight()
+    canvas_width = percentageWindow(canvas_width_percentage, window_width)
+    canvas_height = percentageWindow(canvas_height_percentage, window_height)
+    canvas.create_window((canvas_width, canvas_height), window=frame, anchor=CENTER)
+    return frame
 def quiz_window(window, num):
     #Visuals
     fileName = os.getcwd() + '\\Studysets.csv'
@@ -142,17 +169,7 @@ def quiz_window(window, num):
     fullyCorrectScore = len(list(questionMC)) + len(list(questionTF)) + len(list(questionTFWrong)) #Adding len of questionMC, questionTF, questionTFWrong to find total score for the quiz
     start = time.time() #Start timer
     while questionMC != {}:
-        frame = Frame(canvas)
-        canvas_width_percentage = 50  # Width as a percentage of the window width
-        canvas_height_percentage = 50  # Height as a percentage of the window height
-        # Calculate the pixel values based on percentages
-        window_width = window.winfo_screenwidth()
-        window_height = window.winfo_screenheight()
-        
-        canvas_width = percentageWindow(canvas_width_percentage, window_width)
-
-        canvas_height = percentageWindow(canvas_height_percentage, window_height)
-        canvas.create_window((canvas_width, canvas_height), window=frame, anchor=CENTER)
+        frame = frames(canvas, window)
 
         title = "Quiz"
         titlelbl = Label(frame, text = title)
@@ -188,15 +205,8 @@ def quiz_window(window, num):
             score += 1
         frame.destroy()
     while questionsTF !={}:
-        frame = Frame(canvas)
-        canvas_width_percentage = 50  # Width as a percentage of the window width
-        canvas_height_percentage = 50  # Height as a percentage of the window height
-        # Calculate the pixel values based on percentages
-        window_width = window.winfo_screenwidth()
-        window_height = window.winfo_screenheight()
-        canvas_width = percentageWindow(canvas_width_percentage, window_width)
-        canvas_height = percentageWindow(canvas_height_percentage, window_height)
-        canvas.create_window((canvas_width, canvas_height), window=frame, anchor=CENTER)
+        frame = frames(canvas, window)
+
 
         dictQuestionsTF = dict(questionsTF) #Temporary variable to store questionMC, as when we pop questions from questionMC, we need to still be able to call the answers
 
@@ -229,43 +239,64 @@ def quiz_window(window, num):
     end = time.time()
     timeCompletion = end-start
     scorePercentage = str((score/fullyCorrectScore)*100)
-    print("Time taken to complete the quiz: ", timeCompletion, "seconds")
-    print("score: ", scorePercentage + "%")
     leaderboard(canvas, fileName, num, timeCompletion, scorePercentage)
 def updateLeaderboard(leaderboardFile, num, time, score):
     temp_file = "temp.csv"  # Temporary file to store modified data
-
     with open(leaderboardFile, 'r') as file:
         lines = file.readlines()  # Read all lines from the CSV file
 
     # Modify the desired line with the new data
     scoreEntry = score + "|" + str(time)
-    print(lines)
-    print("num: ", num)
-    print(lines[num])
     lines[num] = lines[num].strip() + "|" + scoreEntry + "\n"
-
+    lineSeperatedList = lines[num].split("|")
+    lineSeperatedList[-1] = (lineSeperatedList[-1]).strip()
+    title = lineSeperatedList[0]
+    scores, times = seperateTermsDefinitions(lineSeperatedList)
+    print("scores: ", scores)
+    orderedScores, orderedTimes = bubbleSort(scores, times)
+    print("orderedScores; ", orderedScores)
+    print("orderedTimes:", orderedTimes)
+    lines[num] = title
+    for i in range(len(orderedScores)):
+        lines[num] = lines[num].strip() + "|" + orderedScores[i] + "|" + orderedTimes[i] +"\n"
     with open(temp_file, 'w') as file:
         file.writelines(lines)  # Write all modified lines to the temporary file
 
     # Replace the original file with the modified file
+
     os.remove(leaderboardFile)
     os.rename(temp_file, leaderboardFile)
-def bubbleSort(arr):
+    return orderedScores, orderedTimes
+def bubbleSort(arr, secondArr):
     #outer loop neeeds to run one time less than the length of the list
     #this loop dictates the number of times we push elements to the end
+    print(arr, secondArr)
     for i in range(len(arr)-1):
         print("i: ", i)
         # innner loop responsible for swaps
         for j in range(len(arr) - i - 1):
-            print("j: ", j)
             #swap element
-            if arr[j] < arr[j+1]:
+            if float(arr[j]) < float(arr[j+1]): #If not float(), would order it based on lexicographic order
                 temp = arr[j] #hold left variable
+                tempTwo = secondArr[j]
                 arr[j] = arr[j+1]#replaces left with right
+                secondArr[j] = secondArr[j+1]#replaces left with right
                 arr[j+1] = temp#replaces right with left
-                print("swap")
-            print(arr)
+                secondArr[j+1] = tempTwo
+    secondArrModified = bubbleSortTime(arr, secondArr)
+    return arr, secondArrModified
+def bubbleSortTime(arr, secondArr):
+    for i in range(len(arr)-1):
+        print("i: ", i)
+        # innner loop responsible for swaps
+        for j in range(len(arr) - i - 1):
+            #swap element
+            if float(arr[j]) == float(arr[j+1]): #If not float(), would order it based on lexicographic order
+                if float(secondArr[j]) > float(secondArr[j+1]):
+                    tempTwo = secondArr[j]
+                    secondArr[j] = secondArr[j+1]#replaces left with right
+                    secondArr[j+1] = tempTwo
+    return secondArr
 def leaderboard(window, fileName, num, time, score):
     leaderboardFile = os.getcwd() + "\StudyletLeaderboard.csv"
     isFile = os.path.isfile(leaderboardFile)
@@ -280,7 +311,7 @@ def leaderboard(window, fileName, num, time, score):
             print(title)
             fileLB.writelines(title + "\n")
         fileLB.close()
-    updateLeaderboard(leaderboardFile, num, time, score)
+    orderedScores, orderedTimes = updateLeaderboard(leaderboardFile, num, time, score)
     canvas = Canvas(window)
     canvas.pack(side=LEFT, fill=BOTH, expand=True)
     frame = Frame(canvas)
@@ -294,36 +325,27 @@ def leaderboard(window, fileName, num, time, score):
 
     canvas_height = percentageWindow(canvas_height_percentage, window_height)
     canvas.create_window((canvas_width, canvas_height), window=frame, anchor=CENTER)
-    
+
     title = "Quiz Leaderboard"
     titlelbl = Label(frame, text = title)
     titlelbl.pack()
+    tree = Treeview(frame)
+    tree["columns"] = ("Score", "Time")
+    tree.column("#0", width=0, stretch=NO)  # Adjust the width of the default column
+    
+    # Define column headings
+    tree.heading("#0", text="", anchor=W)  # Specify an empty string for the default column heading
+    tree.heading("Score", text="Score")
+    tree.heading("Time", text="Time")
+    for i in range(len(orderedScores)):
+    
+    # Insert data rows
+      tree.insert("", END, values=(str(round(float(orderedScores[i]), 2)) + "%", str(round(float(orderedTimes[i]), 2)) + " " + "seconds" ))
+      
+    
+    # Pack the Treeview widget
+    tree.pack()
     backbtn = Button(frame, text = 'Back',command = lambda:[homeMenu(canvas)])
-    backbtn.pack()
-def quizMenu(window):
-    window.delete('all')
-    # Toplevel object which will be treated as a new window
-    canvas_width_percentage = 50 # Width as a percentage of the window width
-    canvas_height_percentage = 40  # Height as a percentage of the window height
-
-    # Calculate the pixel values based on percentages
-    window_width = window.winfo_screenwidth()
-    window_height = window.winfo_screenheight()
-    canvas_width = percentageWindow(canvas_width_percentage, window_width)
-    canvas_height = percentageWindow(canvas_height_percentage, window_height)
-    # Create a Canvas widget
-    canvas = Canvas(window)
-    canvas.pack(side=LEFT, fill=BOTH, expand=True)
-    frame = Frame(canvas)
-    canvas.create_window((canvas_width, canvas_height), window=frame, anchor=CENTER)
-
-    title = "Quiz Menu"
-    titlelbl = Label(frame, text = title)
-    playbtn = Button(frame, text = 'Play quizzes', command = lambda:[playQuizzesMenu(canvas)])
-    backbtn = Button(frame, text = 'Back',
-                     command = lambda:[homeMenu(canvas)])
-    titlelbl.pack()
-    playbtn.pack()
     backbtn.pack()
 def playQuizzesMenu(window):
     folder = os.getcwd()
@@ -343,25 +365,26 @@ def playQuizzesMenu(window):
     canvas.pack(side=LEFT, fill=BOTH, expand=True)
 
     # Create a Scrollbar widget
-    scrollbar = Scrollbar(root, orient=VERTICAL, command=canvas.yview)
-    scrollbar.pack(side=RIGHT, fill=Y)
+    #scrollbar = Scrollbar(root, orient=VERTICAL, command=canvas.yview)
+    #scrollbar.pack(side=RIGHT, fill=Y)
 
     # Configure the Canvas to use the Scrollbar
-    canvas.configure(yscrollcommand=scrollbar.set)
+    #canvas.configure(yscrollcommand=scrollbar.set)
 
     # Create a Frame inside the Canvas
     frame = Frame(canvas)
-    canvas.create_window((canvas_width, canvas_height), window=frame, anchor=NW)
-
-    titlelbl = Label(frame, text = 'Remove Available Studysets')
-    titlelbl.pack
+    canvas.create_window((canvas_width, canvas_height), window=frame, anchor=CENTER)
+    quizTitle = Label(frame, text = "Quizzes")
+    quizTitle.pack()
+    quizDescription = Label(frame, text = "Choose a study set to create a quiz for. The study set must have atleast 4 terms to create a quiz")
+    quizDescription.pack()
     backbutton = Button(frame, text = "back",
-                        command = lambda:[studysetMenu(canvas), toggle_scrollbar(scrollbar)])
+                        command = lambda:[homeMenu(canvas)])
     
     # Placing buttons
     for i in range(lines):
             button = Button(frame, text = titles(i),
-                            command = lambda id = i:[quiz_window(canvas, id), toggle_scrollbar(scrollbar)])
+                            command = lambda id = i:[quiz_window(canvas, id)])
             button.pack(side = "top", fill = X)
     backbutton.pack()
     f.close()
@@ -389,7 +412,7 @@ def homeMenu(window):
     flashcardbtn = Button(frame, text = 'Flashcard',
                         command = lambda:[flashcardHome(canvas)])
     quizbtn = Button(frame, text = 'Quiz',
-                    command = lambda:[quizMenu(canvas)])
+                    command = lambda:[playQuizzesMenu(canvas)])
     studysetbtn = Button(frame, text = "Studysets",
                          command = lambda:[studysetMenu(canvas)])
     titlelbl.pack()
--- test2.py
+++ test2.py
@@ -1,26 +1,27 @@
-import os
+import tkinter as tk
+from tkinter import ttk
 
-def edit_csv_line(csv_file, num, new_data):
-    temp_file = "temp.csv"  # Temporary file to store modified data
+def create_table():
+    root = tk.Tk()
+    root.title("Table Example")
+    
+    # Create Treeview widget
+    tree = ttk.Treeview(root)
+    tree["columns"] = ("column1", "column2", "column3")
+    
+    # Define column headings
+    tree.heading("column1", text="Column 1")
+    tree.heading("column2", text="Column 2")
+    tree.heading("column3", text="Column 3")
+    
+    # Insert data rows
+    tree.insert("", tk.END, values=("Data 1", "Data 2", "Data 3"))
+    tree.insert("", tk.END, values=("Data 4", "Data 5", "Data 6"))
+    
+    # Pack the Treeview widget
+    tree.pack()
 
-    with open(csv_file, 'r') as file:
-        lines = file.readlines()  # Read all lines from the CSV file
+    root.mainloop()
 
-    # Modify the desired line with the new data
-    print(lines[num])
-    print(lines[num-1])
-    lines[num - 1] = ','.join(new_data) + '\n'
-
-    with open(temp_file, 'w') as file:
-        file.writelines(lines)  # Write all modified lines to the temporary file
-
-    # Replace the original file with the modified file
-    os.remove(csv_file)
-    os.rename(temp_file, csv_file)
-
-# Example usage:
-csv_file = "StudyletLeaderboard.csv"
-num = 1
-new_data = ["John", "Doe", "30", "john.doe@example.com"]
-
-edit_csv_line(csv_file, num, new_data)
+# Call the function to create the table
+create_table()
